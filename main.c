cc7da1a62d516f21644bb275bd5b9f6678f5cd71
Tetsuro Ando <teando@c2r9s2.42tokyo.jp>
2024-11-21 15:14:53 +0900

    fix 2free bag

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 19:51:04 by teando            #+#    #+#             */
-/*   Updated: 2024/11/18 11:10:51 by teando           ###   ########.fr       */
+/*   Updated: 2024/11/21 14:56:26 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,19 +26,17 @@ int	main(void)
 	i = -1;
 	j = 0;
 	fd[0] = open("test.txt", O_RDONLY);
-	fd[1] = open("1.txt", O_RDONLY);
-	fd[2] = open("2.txt", O_RDONLY);
+	// fd[1] = open("1.txt", O_RDONLY);
+	// fd[2] = open("2.txt", O_RDONLY);
 	// fd[0] = 0;
 	while (j < 1)
 	{
 		printf("file is open: fd=%d\n===\n", fd[j]);
-		while (++i < 50)
+		while (++i < 15)
 		{
 			printf("i = %d\n", i);
-			r = get_next_line(fd[i % 3]);
+			r = get_next_line(fd[j]);
 			printf("%s", r);
-			// if (r == NULL)
-			// 	break ;
 			free(r);
 		}
 		close(fd[j]);
